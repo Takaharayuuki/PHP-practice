407bdd10857a9a2d8f8e0aae3e6f5caa54c9a873
Takaharayuuki <yuuki.barber@gmail.com>
2021-02-12 17:12:46 +0900

    add: Humanクラス

---
+++ PHP-Object-orientation/classes/Human.php
@@ -0,0 +1,26 @@
+<?php
+
+class Human
+{
+  // プロパティ
+  const MAX_HITPOINT = 100;
+  public $name;
+  public $hitPoint = 100;
+  public $attackPoint = 20;
+
+  // メソッド
+  public function doAttack($enemy)
+  {
+    echo "『" .$this->name . "』の攻撃！\n";
+    echo "【" .$enemy->name . "】に" .$this->attackPoint . "のダメージ！\n";
+    $enemy->tookDamage($this->attackPoint);
+  }
+
+  public function tookDamege($damage)
+  {
+    $this->hitPoint -= $damage;
+    if ($this->hitPoint < 0){
+        $this->hitPoint = 0;
+    }
+  }
+}
