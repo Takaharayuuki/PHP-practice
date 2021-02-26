c9152db500c1c99103b1127cff529a83f2d42a63
Takaharayuuki <yuuki.barber@gmail.com>
2021-02-26 12:08:05 +0900

    Add: Aboutページの追加 web.php router.js ルーティング

--- laravel_vue_router/routes/web.php
+++ laravel_vue_router/routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/{any}', function () {
+    return view('welcome');
+})->where('any','.*');
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
