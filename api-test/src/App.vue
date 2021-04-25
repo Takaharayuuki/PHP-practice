c361a80b0bb916d897545765b791199fe5b97771
yuuki_takahara <takahara.yuuki22@gmail.com>
2021-04-25 21:13:16 +0900

    axios テスト

--- api-test/src/App.vue
+++ api-test/src/App.vue
@@ -1,16 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="search-ip">
+    <div>
+      <div>
+        <input @click="getIp" type="button" value="IPを取得" />
+      </div>
+      <h3>IP</h3>
+      <p>{{ state.data }}</p>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent, reactive } from "vue";
+import axios from "axios";
 
 export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
+  name: "SearchIp",
+  setup() {
+    const state = reactive<{ data: string }>({
+      data: "",
+    });
+
+    const getIp = () => {
+      axios
+        .get("https://httpbin.org/get")
+        .then((response) => {
+          alert(response.data.origin);
+          state.data = response.data.origin;
+          return state.data;
+        })
+        .catch((e) => {
+          alert(e);
+        });
+    };
+
+    return {
+      state,
+      getIp,
+    };
   },
 });
 </script>
