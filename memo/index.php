876f6b199149ec8c3ec348b5e056a8244e5fb273
yuuki_takahara <takahara.yuuki22@gmail.com>
2021-05-06 22:14:26 +0900

    カラム名を指定して欲しいデータを取得

--- memo/index.php
+++ memo/index.php
@@ -16,9 +16,14 @@
     echo 'DB接続エラー : ' . $e->getMessage();
   }
 
-  $count = $db->exec('INSERT INTO my_items SET maker_id=1, item_name="もも",item_name_kana="モモ",price=210, keyword="缶詰、ピンク、甘い",sales=2');
-  print_r($db->errorInfo());
-  echo $count . '件のデータを追加';
+  // $count = $db->exec('INSERT INTO my_items SET maker_id=1, item_name="もも",item_name_kana="モモ",price=210, keyword="缶詰、ピンク、甘い",sales=2');
+  // print_r($db->errorInfo());
+  // echo $count . '件のデータを追加';
+  $records = $db->query('SELECT * FROM my_items');
+  while ($record = $records->fetch()) {
+    print($record['item_name'] . "\n");
+  }
+
   ?>
 </body>
 
